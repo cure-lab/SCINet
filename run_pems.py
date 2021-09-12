ce3dfe5f8289c6bab0b454f01284f3499f625bf0
ailingzengzzz <32029490+ailingzengzzz@users.noreply.github.com>
2021-09-12 22:22:29 +0800

    Update run_pems.py

--- run_pems.py
+++ run_pems.py
@@ -9,25 +9,37 @@ import numpy as np
 from tensorboardX import SummaryWriter
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--train', type=bool, default=False)
-parser.add_argument('--evaluate', type=bool, default=True)
-parser.add_argument('--finetune', type=bool, default=False)
-parser.add_argument('--dataset', type=str, default='PEMS08')  #PeMS07
-parser.add_argument('--window_size', type=int, default=12)
-parser.add_argument('--horizon', type=int, default=12)
-parser.add_argument('--train_length', type=float, default=6)
-parser.add_argument('--valid_length', type=float, default=2)
-parser.add_argument('--test_length', type=float, default=2)
-parser.add_argument('--epoch', type=int, default=50)
-parser.add_argument('--lr', type=float, default=0.001)
+### -------  dataset settings --------------
+parser.add_argument('--dataset', type=str, default='PEMS08', choices=['PEMS03', 'PEMS04', 'PEMS07', 'PEMS08')  #sometimes use: PeMS08
+parser.add_argument('--norm_method', type=str, default='z_score')
+parser.add_argument('--input_dim', type=int, default=170, help='the variants of data')
+parser.add_argument('--normtype', type=int, default=0)
+
+### -------  device settings --------------
 parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
 parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
 parser.add_argument('--gpu', type=int, default=0, help='gpu')
-
 parser.add_argument('--device', type=str, default='cuda:0')
+                                                                      
+### -------  input/output length settings --------------                                                                            
+parser.add_argument('--window_size', type=int, default=12)
+parser.add_argument('--horizon', type=int, default=12)
+parser.add_argument('--num_concat', type=int, default=21)
+parser.add_argument('--single_step_output_One', type=int, default=0)
+
+parser.add_argument('--train_length', type=float, default=6)
+parser.add_argument('--valid_length', type=float, default=2)
+parser.add_argument('--test_length', type=float, default=2)
+
+### -------  training settings --------------  
+parser.add_argument('--train', type=bool, default=True)
+parser.add_argument('--evaluate', type=bool, default=True)
+parser.add_argument('--finetune', type=bool, default=False)
 parser.add_argument('--validate_freq', type=int, default=1)
+
+parser.add_argument('--epoch', type=int, default=50)
+parser.add_argument('--lr', type=float, default=0.001)
 parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--norm_method', type=str, default='z_score')
 parser.add_argument('--optimizer', type=str, default='N') #
 parser.add_argument('--early_stop', type=bool, default=False)
 parser.add_argument('--exponential_decay_step', type=int, default=5)
@@ -35,24 +47,16 @@ parser.add_argument('--decay_rate', type=float, default=0.5)
 
 parser.add_argument('--lradj', type=int, default=6,help='adjust learning rate')
 parser.add_argument('--weight_decay', type=float, default=1e-5)
-parser.add_argument('--model_name', type=str, default='Normal')
-# Action Part
-parser.add_argument('--input_dim', type=int, default=170)################
+parser.add_argument('--model_name', type=str, default='SCINet')
+
+### -------  model settings --------------  
 parser.add_argument('--num_stacks', type=int, default=1)
-
-parser.add_argument('--hidden-size', default=1, type=float, help='hidden channel of module')###################################
-parser.add_argument('--INN', default=1, type=int, help='use INN or basic strategy')###########################
-
+parser.add_argument('--hidden-size', default=1, type=float, help='hidden channel of module')
+parser.add_argument('--INN', default=1, type=int, help='use INN or basic strategy')
 parser.add_argument('--kernel', default=5, type=int, help='kernel size')
 parser.add_argument('--dilation', default=1, type=int, help='dilation')
 parser.add_argument('--positionalEcoding', type = bool , default=False)
-parser.add_argument('--num_concat', type=int, default=21)
-parser.add_argument('--normtype', type=int, default=0)
-
-
-#SCINet
 parser.add_argument('--groups', type=int, default=1)
-parser.add_argument('--single_step_output_One', type=int, default=0)
 parser.add_argument('--layers', type=int, default=3)
 
 args = parser.parse_args()
