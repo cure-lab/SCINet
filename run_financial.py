4e9174e4a2f887ea4067389519c79724f05374d2
ailingzengzzz <32029490+ailingzengzzz@users.noreply.github.com>
2021-09-12 22:13:11 +0800

    Update run_financial.py

--- run_financial.py
+++ run_financial.py
@@ -9,57 +9,53 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
 parser = argparse.ArgumentParser(description='PyTorch Time series forecasting')
-parser.add_argument('--data', type=str, default='./datasets/financial/exchange_rate.txt',
+### -------  dataset settings --------------
+parser.add_argument('--data', type=str, default='./datasets/exchange_rate.txt',
                     help='location of the data file')
+parser.add_argument('--dataset_name', type=str, default='electricity')
+parser.add_argument('--normalize', type=int, default=2)
+
+### -------  device settings --------------
+parser.add_argument('--device',type=str,default='cuda:0',help='')
+parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+
+### -------  input/output length settings --------------                                                                            
+parser.add_argument('--window_size', type=int, default=168) # input size
+parser.add_argument('--horizon', type=int, default=3)  # predication
+parser.add_argument('--single_step', type=int, default=0)
+parser.add_argument('--single_step_output_One', type=int, default=0)
+parser.add_argument('--lastWeight', type=float, default=1.0,help='Loss weight lambda')
+
+### -------  training settings --------------  
+parser.add_argument('--train', type=bool, default=True)
 parser.add_argument('--log_interval', type=int, default=2000, metavar='N',
                     help='report interval')
 parser.add_argument('--save', type=str, default='model/model.pt',
                     help='path to save the final model')
 parser.add_argument('--optim', type=str, default='adam')
 parser.add_argument('--L1Loss', type=bool, default=True)
-parser.add_argument('--normalize', type=int, default=2)
-parser.add_argument('--device',type=str,default='cuda:0',help='')
-
 parser.add_argument('--num_nodes',type=int,default=8,help='number of nodes/variables')
-
-parser.add_argument('--train', type=bool, default=True)
-parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
-parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
-parser.add_argument('--gpu', type=int, default=0, help='gpu')
-
 parser.add_argument('--batch_size',type=int,default=8,help='batch size')
 parser.add_argument('--lr',type=float,default=5e-3,help='learning rate')
 parser.add_argument('--weight_decay',type=float,default=0.00001,help='weight decay rate')
 parser.add_argument('--epochs',type=int,default=100,help='')
+parser.add_argument('--lradj', type=int, default=6,help='adjust learning rate')
+parser.add_argument('--save_path', type=str, default='./SingleStepCheckpoint')
+
+### -------  model settings --------------  
 parser.add_argument('--hidden-size', default=1.0, type=float, help='hidden channel of module')# H, EXPANSION RATE
 parser.add_argument('--INN', default=1, type=int, help='use INN or basic strategy')
 parser.add_argument('--kernel', default=5, type=int, help='kernel size')#k kernel size
 parser.add_argument('--dilation', default=1, type=int, help='dilation')
-parser.add_argument('--lradj', type=int, default=6,help='adjust learning rate')
-
 parser.add_argument('--model_name', type=str, default='EncoDeco')
 parser.add_argument('--model_mode', type=str, default='EncoDeco')
 parser.add_argument('--positionalEcoding', type = bool , default=False)
-
-parser.add_argument('--window_size', type=int, default=168) # input size
-parser.add_argument('--horizon', type=int, default=3)  # predication
-
 parser.add_argument('--dropout', type=float, default=0.5)
-
 parser.add_argument('--num_concat', type=int, default=165)
-
-parser.add_argument('--single_step', type=int, default=0)
-parser.add_argument('--single_step_output_One', type=int, default=0)
-parser.add_argument('--lastWeight', type=float, default=1.0,help='Loss weight lambda')
-
 parser.add_argument('--groups', type=int, default=1)
 parser.add_argument('--layers', type=int, default=3)
-
-parser.add_argument('--save_path', type=str, default='./SingleStepCheckpoint')
-parser.add_argument('--dataset_name', type=str, default='electricity')
-
-
-
 
 args = parser.parse_args()
 
